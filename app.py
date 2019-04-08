2f3f031cb5b0fd3a9d7356a6e20565fbd843481f
Paul Wilson <pwilson802@gmail.com>
2019-04-08 22:47:39 +1000

    adding congit.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -9,7 +13,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-    
+
 
 if __name__ == '__main__':
     app.run()
